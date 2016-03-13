49ccccf1551825dbd72acfcf3a299a8c4ecb8374
Marijn <marijnfs@stream>
2016-03-13 15:15:29 +0100

    contrast norm

--- mandel.go
+++ mandel.go
@@ -4,18 +4,20 @@ import (
 	"os"
 	"fmt"
 	img "image"
-	"image/color"
+	//"image/color"
+	"image/color/palette"
 	"image/png"
 	"math"
+	"sort"
 )
 
-const IT = 512
+const IT = 2000
 
 func it(ca, cb float64) (int, float64) {
 	var a, b float64 = 0, 0
 	for i := 0; i < IT; i++ {
 		as, bs := a*a, b*b
-		if as + bs > 6 {
+		if as + bs > 4 {
 			return i, as + bs
 		}
 		//if as + bs < .00001 {
@@ -27,15 +29,22 @@ func it(ca, cb float64) (int, float64) {
 }
 
 func main() {
-	width, height := 4000, 2000
+	//width, height := 1366*2, 768*2
+	width, height := 1366, 768
 	ratio := float64(height) / float64(width)
-	xpos, ypos, zoom_width := .275, .4775, .01
+	//xpos, ypos, zoom_width := .275, .4775, .01
+	//xpos, ypos, zoom_width := -.750045367143, 0.004786271734, .000000001
+	//xpos, ypos, zoom_width := -.6286, -.0062, 1.
+	xpos, ypos, zoom_width := -.0062, -.6486, .1
 	xmin, xmax := xpos - zoom_width / 2.0, xpos + zoom_width / 2.0
 	ymin, ymax := ypos - zoom_width * ratio / 2.0, ypos + zoom_width * ratio / 2.0
 	
+	
+	single_values := make([]float64, width * height)
+	
+	fmt.Println("Mandelling...")
 
-	fmt.Println("hello")
-	image := img.NewRGBA(img.Rectangle{img.Point{0, 0}, img.Point{width, height}})
+	i := 0
 	for y := 0; y < height; y++ {
 		for x := 0; x < width; x++ {
 			a := (float64(x) / float64(width)) * (xmax - xmin) + xmin 
@@ -43,15 +52,43 @@ func main() {
 			stop_it, norm := it(a, b)
 			smooth_val := IT + 1 - (math.Log(norm) + float64(stop_it))
 			smooth_val /= IT
+			single_values[i] = smooth_val
 			//fmt.Println(norm)
-			r, g, b := HuslToRGB(smooth_val * 3600., 100.0, smooth_val * 100.0)
+			//r, g, b := HuslToRGB(100. + 100. * smooth_val, 88.7, 44.3 + 20. * smooth_val)
+			//r, g, b := smooth_val, .4 * smooth_val, -smooth_val
 			//fmt.Println(norm, stop_it, smooth_val)
-			c := color.RGBA{uint8(255. * r), uint8(255. * g), uint8(255. * b), 255}
+			//c := color.RGBA{uint8(255. * r), uint8(255. * g), uint8(255. * b), 255}
 			
-			image.Set(x, y, c)
+			//image.Set(x, y, c)
+			i++
 		}
 	}
 
+	sorted_values := make([]float64, len(single_values))
+	for i := range sorted_values {
+		sorted_values[i] = single_values[i]
+	}
+	sort.Float64s(sorted_values)
+	//fmt.Println(sorted_values[0:10])
+
+	pal := palette.Plan9
+	split_values := make([]float64, len(pal)-1)
+	for i := range split_values {
+		split_values[i] = sorted_values[i * len(sorted_values) / len(split_values)]
+	}
+	//fmt.Println(split_values)
+	
+
+	image := img.NewRGBA(img.Rectangle{img.Point{0, 0}, img.Point{width, height}})
+	i = 0
+	for y := 0; y < height; y++ {
+		for x := 0; x < width; x++ {
+			color_index := sort.Search(len(split_values), func(j int) bool {return single_values[i] < split_values[j]})
+			//fmt.Println(color_index)
+			image.Set(x, y, pal[color_index])
+			i++
+		}
+	}
 	out_file, _ := os.Create("out.png")
 	png.Encode(out_file, image)
 }
